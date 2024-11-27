71e89993995a979f2ebc27c7ed9336b37ee2e3f3
Siddharth chandrakar <siddharthchandrakar007@gmail.com>
2024-11-27 23:42:13 +0530

    fix:insert in cart

--- config/database.go
+++ config/database.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"core/models"
 	"fmt"
 	"log"
 
@@ -16,8 +15,7 @@ var err error
 
 func DbInit() {
 	config := GetConfig()
-	connectString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		config.DbHost, config.DbUser, config.DbPassword, config.DbName, config.DbPort)
+	connectString := fmt.Sprintf(config.Dburl)
 
 	// Open the connection to the database
 	db, err = gorm.Open(postgres.Open(connectString), &gorm.Config{
@@ -34,10 +32,10 @@ func DbInit() {
 	fmt.Println("Connected to Database")
 
 	// Uncomment if you have models to migrate
-	err = db.AutoMigrate(&models.Cart{})
-	if err != nil {
-		log.Fatalf("Migration failed: %v", err)
-	}
+	// err = db.AutoMigrate(&models.User{}, &models.BookSummary{},&models.Cart{},&models.Books{})
+	// if err != nil {
+	// 	log.Fatalf("Migration failed: %v", err)
+	// }
 	// fmt.Println(config.DbHost, config.DbName)
 }
 
